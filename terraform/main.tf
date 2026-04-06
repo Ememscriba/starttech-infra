4d10ed0cc90b19aa4176024a3e0f9c7a77f9f0d1
John <johnvictoshi@gmail.com>
2026-04-06 19:40:08 +0100

    feat: add infrastructure pipeline and remote state backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,22 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  required_providers {
-  aws = {
-    source  = "hashicorp/aws"
-    version = "~> 5.0"
+  backend "s3" {
+    bucket = "starttech-terraform-state-954692413962"
+    key    = "production/terraform.tfstate"
+    region = "us-east-1"
   }
-  random = {
-    source  = "hashicorp/random"
-    version = "~> 3.0"
-  }
-}
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 }
 
 provider "aws" {
